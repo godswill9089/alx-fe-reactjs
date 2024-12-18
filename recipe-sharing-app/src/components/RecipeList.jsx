70e3fae01ad39c872c7ebb180ac64646025f3ba6
ejimofor godswill <ejimoforgodswill60@gmail.com>
2024-12-18 12:21:22 +0100

    first commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import useRecipeStore from './RecipeStore';
+import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipe);
