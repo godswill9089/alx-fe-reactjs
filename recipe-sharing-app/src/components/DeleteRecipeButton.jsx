5414563db29c4e62bd7f3af95fa08b4b3578a602
ejimofor godswill <ejimoforgodswill60@gmail.com>
2024-12-18 13:08:16 +0100

    first commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
 const DeleteRecipeButton = ({ id }) => {
-  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+     const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(id);
-    alert("Recipe deleted!");
+      alert("Recipe deleted!");
+       navigate("/");
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
