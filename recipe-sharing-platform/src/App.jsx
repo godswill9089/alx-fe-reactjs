98d875e78364f5cc48ba68f0d8edf8cf3d07bf68
ejimofor godswill <ejimoforgodswill60@gmail.com>
2024-12-19 06:15:55 +0100

    first commit

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="text-blue-500 text-center mt-10">
+      <h1 className="text-3xl font-bold">Welcome to Recipe Sharing Platform</h1>
+      <p className="text-lg">Tailwind CSS is successfully integrated!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
