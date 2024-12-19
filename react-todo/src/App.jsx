6057f26008b7a240ae7bad603b7407739bbbc6fe
ejimofor godswill <ejimoforgodswill60@gmail.com>
2024-12-19 05:51:30 +0100

    first commit

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './componets/TodoList'; // Ensure the correct relative path
+
+const App = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
