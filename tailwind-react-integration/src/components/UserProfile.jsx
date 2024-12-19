4c2d9b09d229de500dc032a3e5d6e44debb7c738
ejimofor godswill <ejimoforgodswill60@gmail.com>
2024-12-19 06:03:23 +0100

    first commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
@@ -8,8 +8,7 @@ function UserProfile() {
       />
       <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-sm sm:text-base text-gray-600">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
+        Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
